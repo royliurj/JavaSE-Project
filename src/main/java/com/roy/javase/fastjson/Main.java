64cf74da53c2d8fd9264377cea90449e953be148
liurj <liuranjun@sinopatho.com>
2019-04-26 14:30:51 +0800

    JSON 反序列化错误

--- src/main/java/com/roy/javase/fastjson/Main.java
+++ src/main/java/com/roy/javase/fastjson/Main.java
@@ -2,6 +2,7 @@ package com.roy.javase.fastjson;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.roy.javase.fastjson.model.BadSuccessDataTip;
 import com.roy.javase.fastjson.model.SuccessDataTip;
 import com.roy.javase.fastjson.model.SuccessTip;
 
@@ -12,6 +13,15 @@ import com.roy.javase.fastjson.model.SuccessTip;
 public class Main {
 
     public static void main(String[] args) {
+//        BadSuccessDataTip successTip = new BadSuccessDataTip("111");
+//        successTip.setCode(222);
+//
+//        String json = JSON.toJSONString(successTip);
+//        System.out.println(json);
+//
+//        BadSuccessDataTip tip = JSONObject.parseObject(json , BadSuccessDataTip.class);
+//        System.out.println(tip);
+
         SuccessDataTip successTip = new SuccessDataTip("111");
         successTip.setCode(222);
 
